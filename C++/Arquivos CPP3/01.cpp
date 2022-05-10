780a4892523ba944161b028af0c24b9720337423
StPfeffer <mateuspfeffer@gmail.com>
2022-05-10 16:20:33 -0300

    Update 01.cpp

--- C++/Arquivos CPP3/01.cpp
+++ C++/Arquivos CPP3/01.cpp
@@ -10,9 +10,36 @@ using namespace std;
 int main() {
     SetConsoleOutputCP(65001);
 
-    int aulas;
-    float media, presença;
+    int aulas, presencas;
+    float media, nota1, nota2, nota3, nota4, percentual_presenca;
     string nome;
+
+    cout << "Insira o nome do aluno: ";
+    cin >> nome;
+    cout << "Primeira nota: ";
+    cin >> nota1;
+    cout << "Segunda nota: ";
+    cin >> nota2;
+    cout << "Terceira nota: ";
+    cin >> nota3;
+    cout << "Quarta nota: ";
+    cin >> nota4;
+    cout << "Informe a quantidade de aulas da matéria: ";
+    cin >> aulas;
+    cout << "Quantidade de presenças do aluno: ";
+    cin >> presencas;
+
+    percentual_presenca = (presencas * 100) / aulas;
+    media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    if (percentual_presenca >= 75.0 && media >= 7.0) {
+        cout << "Nome: " << nome;
+        cout << "Média: " << media;
+        cout << "Aprovado";
+    } else {
+        cout << "Nome: " << nome;
+        cout << "Reprovado";
+    }
 
     return 0;
 }
