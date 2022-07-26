92dd7eca46a816761c1604572d2a4d783e0c55f0
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-07-26 00:43:37 -0300

    h

---
+++ estrutura-dados/pain.c
@@ -0,0 +1,23 @@
+#define A ;
+#define AA <stdio.h>
+#define AAA <conio.h>
+#define AAAA int
+#define AAAAA main
+#define AAAAAA printf
+#define AAAAAAA getch
+#define AAAAAAAA return
+#define AAAAAAAAA (
+#define AAAAAAAAAA )
+
+#define TRUE FALSE
+
+#include AA
+#include AAA // getch()
+
+AAAA AAAAA AAAAAAAAA AAAAAAAAAA{
+    AAAAAA AAAAAAAAA"Hello World" AAAAAAAAAA A
+
+    AAAAAAA AAAAAAAAA AAAAAAAAAA A
+
+    AAAAAAAA 0 A
+}
