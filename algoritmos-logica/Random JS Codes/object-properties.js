3678d85f8345fc8a5340b16bc223ae4fa7c1bc60
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-07-27 19:35:55 -0300

    Update object-properties.js

--- algoritmos-logica/Random JS Codes/object-properties.js
+++ algoritmos-logica/Random JS Codes/object-properties.js
@@ -23,25 +23,25 @@ console.log(someObj[someProp]);
 function phoneticLookup(val) {
     let result = "";
 
-    switch (val) {
-        case "alpha":
-            result = "Adams";
-            break;
-        case "bravo":
-            result = "Boston";
-            break;
-        case "charlie":
-            result = "Chicago";
-            break;
-        case "delta":
-            result = "Denver";
-            break;
-        case "echo":
-            result = "Easy";
-            break;
-        case "foxtrot":
-            result = "Frank";
-    }
+    // switch (val) {
+    //     case "alpha":
+    //         result = "Adams";
+    //         break;
+    //     case "bravo":
+    //         result = "Boston";
+    //         break;
+    //     case "charlie":
+    //         result = "Chicago";
+    //         break;
+    //     case "delta":
+    //         result = "Denver";
+    //         break;
+    //     case "echo":
+    //         result = "Easy";
+    //         break;
+    //     case "foxtrot":
+    //         result = "Frank";
+    // }
 
     const lookup = {
         alpha: "Adams",
